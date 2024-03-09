f71cee70b3ffacfb99a8ece32af3010f163a93e7
Voinkov Alexandr <colorx265@gmail.com>
2024-03-09 23:15:03 +0700

    Created server-client connection

--- main.gd
+++ main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var battle : PackedScene
+
 func _on_host_button_pressed():
 	upnp_setup()
 	var peer = ENetMultiplayerPeer.new()
@@ -8,8 +10,16 @@ func _on_host_button_pressed():
 	
 	load_game()
 	
+func _on_join_button_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(%ConnectionIP.text, 9999)
+	multiplayer.multiplayer_peer = peer
+	
+	multiplayer.connected_to_server.connect(load_game)
+
 func load_game():
 	%Menu.hide()
+	%BattleInstance.add_child(battle.instantiate())
 	
 # Set up port mapping for online multiplayer functionality
 func upnp_setup():
---
+++ scenes/card.gd
@@ -0,0 +1,8 @@
+extends TextureRect
+
+func _on_mouse_entered():
+	pass
+	
+func _on_mouse_exited():
+	pass
+	
